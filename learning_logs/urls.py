1b03ae986de9cc1eaaf624a726da1f9b1719fd67
majidvayghani <mjvayghani@gmail.com>
2023-08-14 14:23:27 +0330

    mapping url

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('learning_logs.urls')),
+]
+
+"""Defines URL patterns for learning_logs."""
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
 ]
