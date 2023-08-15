d4d5c7212c6c5ed6dd3ba212f58141982b8e3253
majidvayghani <mjvayghani@gmail.com>
2023-08-16 00:34:50 +0330

    fix missing ':' after class Meta.

--- learning_logs/form.py
+++ learning_logs/form.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Topic
 
 class TopicForm(forms.ModelForm):
-    class Meta
+    class Meta:
         model = Topic
         fields = ['text']
         labels = {'text':''}
