34bce046d9f72e4da4f3f89bddb8dbf84f65b81d
majidvayghani <mjvayghani@gmail.com>
2023-08-27 22:47:21 +0330

    the login page

--- crach_python/urls.py
+++ crach_python/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls')),
-    path('account/', include('account.urls')),
+    path('account/', include('accounts.urls')),
+]
